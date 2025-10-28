d0463096f061e188df2f74040e7b952627082b70
keeppn <radostin.angelov94@gmail.com>
2025-10-28 14:24:06 +0200

    Fix: Externalize @google/generative-ai

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,10 @@ export default defineConfig({
       alias: {
         '@': path.resolve(__dirname, '.'),
       }
-    }
+    },
+    build: {
+      rollupOptions: {
+        external: ['@google/generative-ai'],
+      },
+    },
 });
