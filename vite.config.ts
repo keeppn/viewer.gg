50702d4318f4c943f1d09a224c202d22246cd12b
keeppn <radostin.angelov94@gmail.com>
2025-10-28 14:41:41 +0200

    Fix: configure PostCSS in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
     server: {
@@ -8,6 +10,14 @@ export default defineConfig({
       host: '0.0.0.0',
     },
     plugins: [react()],
+    css: {
+      postcss: {
+        plugins: [
+          tailwindcss,
+          autoprefixer,
+        ],
+      },
+    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
